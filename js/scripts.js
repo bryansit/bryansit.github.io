0adae4828018d09daf19474dfdddc0b31968f2c1
bryansit <esnipermu@gmail.com>
2024-08-16 15:48:14 -0500

    modificado nivel bonito

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,27 @@ function checkCredentials() {
         messageElement.textContent = 'Nombre o contraseña incorrectos.';
     }
 }
+function openNote() {
+    const note = document.getElementById("note");
+    if (note.style.display === "none" || note.style.display === "") {
+        note.style.display = "block";
+    } else {
+        note.style.display = "none";
+    }
+}
+function openSecondNote() {
+    const noteSecond = document.getElementById("note-second");
+    if (noteSecond.style.display === "none" || noteSecond.style.display === "") {
+        noteSecond.style.display = "block";
+    } else {
+        noteSecond.style.display = "none";
+    }
+}
+function openTopNote() {
+    const noteTop = document.getElementById("note-top");
+    if (noteTop.style.display === "none" || noteTop.style.display === "") {
+        noteTop.style.display = "block";
+    } else {
+        noteTop.style.display = "none";
+    }
+}
